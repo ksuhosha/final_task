88096aec77d691628facc22502348bbacb647902
Lapa <ksuhosha@mail.ru>
2022-10-01 17:54:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите количесво строк:");
+int N= int.Parse(Console.ReadLine());
+string[] array = new string[N];
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"введите {i+1}/{N}");
+    array[i] = Console.ReadLine()!;
+}
