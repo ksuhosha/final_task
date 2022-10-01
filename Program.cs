2cbe21e20f8ec5ce9b484a9681e5c80eef2abcd4
Lapa <ksuhosha@mail.ru>
2022-10-01 20:50:17 +0300

    добавления README.md

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("введите количесво строк:");
-int N= int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
 string[] array = new string[N];
 for (int i = 0; i < N; i++)
 {
-    Console.WriteLine($"введите {i+1}/{N}");
+    Console.WriteLine($"введите {i + 1}/{N}");
     array[i] = Console.ReadLine()!;
 }
 
